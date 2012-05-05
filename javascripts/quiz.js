f4d5b3a01222763b6a0b2c82c83e671a7b5a68e5
GopinathS <gopinath.12345@gmail.com>
2012-05-05 12:52:51 +0530

    Removed the static values ..

--- javascripts/quiz.js
+++ javascripts/quiz.js
@@ -149,11 +149,10 @@ Ember.RadioButton = Ember.View.extend({
   //  var input = this.$('input:radio');
   //  set(this, 'value', input.attr('value'));
   }
-
 });
 
 QuizApp.Models.qqq = Ember.Object.extend({
-    species : ''
+    species : 'hello'
 });
 
 QuizApp.Models.controller = Ember.Object.create({
